fc6ff430db42c8c93508114b7b26323df983607d
vamsivenkat987 <vamsivenkat1998@gmail.com>
2020-05-04 15:27:34 +0200

    second commit on client

--- client.py
+++ client.py
@@ -3,68 +3,56 @@ import socket
 import sys
 import time
 import errno
-import read
+import threading
+import queue
 class client_socket:
     def __init__(self):
         try:
             self.client_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            self.client_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)         
             print('socket created')
         except socket.error :
             print('Socked not created')
             sys.exit()
         self.host = sys.argv[1]
         self.port = int(sys.argv[2])
-        self.nick_name = str(sys.argv[3])
+        self.nick_name = sys.argv[3]
         if(len(sys.argv)!=4):
             sys.exit()
         try:
             self.client_sock.connect((self.host,self.port))
+            self.connected = (self.host,self.port)
             print('client connected')
         except socket.error :
             print('server not found')
             sys.exit()
-        first_message = self.client_sock.recv(2048).decode('utf-8'))
-        print(first_message)
-        self.nick_name = 'NICK' +self.nick_name
         self.nick_name = bytes(self.nick_name ,'utf-8')
         self.client_sock.send(self.nick_name)
-        self.client_sock.setblocking(False)
-        okay_message = self.client_sock.recv(2048).decode('utf-8'))
-        if okay_message == "OK":
-            pass
-        elif okay_message == "Error malformed":
-            print("please enter nick name with in 12 letters with specific characters")
-            sys.exit()
-          
-        
-    def run_client(self):
+        self.client_sock.setblocking(1)
+    def recvdata(self):
         while True:
             try:
-                data = input()
-                data = bytes(data,'utf-8')
-                self.client_sock.send(data)
+                data = self.client_sock.recv(1024)
+                if (len(data)!=0):
+                    print(data)
+                    time.sleep(0.1)
             except IOError:
                 continue
-            list_sock = [sys.stdin , self.client_sock]
-            read_sockets,write_sockets,error_sockets=select.select(list_sock[])
-                
+    def inputdata(self):
+        try:
+            data = input()
+            data = bytes(data,'utf-8')
             try:
-                for i in read_sockets:
-                    if sockets == self.client_sock:
-                    data = self.client_sock.recv(1024)
-                    print(data)
-                else:
-                    data = sys.stdin.readline()
-                    data= ' MSG '+ data
-                    if data == '\n':
-                        continue
-                    else:
-                        self.client_sock.sendall(messgae.encode('utf-8'))
-             
-            except IOError:
-                continue   
-        self.client_sock.close()     
+                self.client_sock.send(data)
+                time.sleep(0.1)
+            except:
+                pass
+        except IOError:
+            pass
+    def run_client(self):
+        t1 = threading.Thread(target = self.recvdata)
+        t1.start()
+        while True:
+            self.inputdata()
 def main():
     client_obj = client_socket()
     client_obj.run_client()
